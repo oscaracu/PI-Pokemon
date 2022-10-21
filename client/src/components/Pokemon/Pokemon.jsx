b83fe95dcf82adfd3a60d5fd9e89c2148968c7ba
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-10-21 08:57:29 +0000

    Pokemon details added

--- client/src/components/Pokemon/Pokemon.jsx
+++ client/src/components/Pokemon/Pokemon.jsx
@@ -1,36 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPokemon } from "../../redux/actions";
+import Loading from "../Loading/Loading";
 
 const Pokemon = (props) => {
+  // Creamos un estado de carga
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
-
+  // Solicitamos los datos a nuestra API
   const dispatch = useDispatch();
   useEffect(() => {
+    setLoading(true);
     dispatch(getPokemon(parseInt(id)));
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
   }, [dispatch, id]);
 
-  const pokemon = useSelector((state) => state.pokemon);
+  const {
+    name,
+    image,
+    types = [],
+    hp,
+    attack,
+    defense,
+    speed,
+    height,
+    weight,
+  } = useSelector((state) => state.pokemon);
 
-  return (
-    <>
-      <div>
-        <img src={pokemon.image} alt="Pokemon" />
-        <h1>{pokemon.name}</h1>
-        <h2>{pokemon.id}</h2>
-        <ul>types</ul>
-        <ul>
-          <li>hp</li>
-          <li>attack</li>
-          <li>defense</li>
-          <li>speed</li>
-        </ul>
-        <p>height</p>
-        <p>weight</p>
-      </div>
-    </>
-  );
+  if (loading) return <Loading />;
+  else
+    return (
+      <>
+        <div>
+          <img src={image} alt="Pokemon" />
+          <h1>{name}</h1>
+          <h2>{id}</h2>
+          <ul>
+            {types.map((type) => (
+              <li key={type.id}>{type.name}</li>
+            ))}
+          </ul>
+          <ul>
+            <li>hp: {hp}</li>
+            <li>attack: {attack}</li>
+            <li>defense: {defense}</li>
+            <li>speed: {speed}</li>
+          </ul>
+          <p>height: {height}</p>
+          <p>weight: {weight}</p>
+        </div>
+      </>
+    );
 };
 
 export default Pokemon;
