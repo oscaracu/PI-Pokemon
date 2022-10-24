1965feb0a09bae838fcb2178ab101bf15fa09c39
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-10-24 10:20:56 +0000

    footer styled

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,28 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  font-family: "Secular One", sans-serif;
+  background-image: url("http://localhost:3001/images/front/red_bg.jpg");
+  padding: 5px;
+  color: #ecf0f1;
+  text-align: center;
+  text-shadow: 2px 2px 5px black;
+  span {
+    color: #f1c40f;
+    font-size: 1.6em;
+    padding: 5px;
+  }
+`;
+
 const Footer = (props) => {
   return (
-    <>
-      <footer>
-        <div>
-          Made with <span>♥</span> by Oscar Cu for SoyHenry
-        </div>
-      </footer>
-    </>
+    <StyledFooter>
+      <div>
+        <p>
+          Made with <span>♥</span> by Oscar Cu | soyHenry WebFT30a_grupo09
+        </p>
+      </div>
+    </StyledFooter>
   );
 };
 
